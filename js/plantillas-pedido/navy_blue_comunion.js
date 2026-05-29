732e5273c1025dbf8602d30ca266326a5830c39e
abuelatata <artesaniaabuelatata@gmail.com>
2026-05-29 19:19:46 +0200

    Add Navy & Blue Comunión template JavaScript file

---
+++ js/plantillas-pedido/navy_blue_comunion.js
@@ -0,0 +1,13 @@
+console.log("Plantilla Navy & Blue Comunión cargada correctamente.");
+
+window.PlantillasPedido = window.PlantillasPedido || {};
+
+window.PlantillasPedido.navy_blue_comunion = {
+  id: "navy_blue_comunion",
+  nombre: "Navy & Blue · Comunión",
+  tipo: "trajes_comunion",
+
+  inicializar: function (coleccion) {
+    console.log("Inicializando plantilla Navy & Blue Comunión:", coleccion);
+  }
+};
